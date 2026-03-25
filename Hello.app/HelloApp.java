df4fd36e870d4a9e49b92cb34ebb4aa224645663
Pritka Sharma <pritkasharma@Pritkas-MacBook-Air.local>
2026-03-25 08:56:46 -0400

    Fix: remove nested git repo

---
+++ Hello.app/HelloApp.java
@@ -0,0 +1,41 @@
+/**
+ * HelloApp - UC4 - A simple Java application that greets multiple users by name
+ * if provided as command-line arguments, or defaults to greeting "World" if no names are given.
+ *
+ * Greet Multiple Users - The application should accept multiple names as command-line
+ * arguments and display a personalized greeting for each user.
+ * Usage: java HelloAppUC4 [name1] [name2] ... [nameN]
+ * - If names are provided, it will display "Hello, [Name1], [Name2], ...!" to the console.
+ * - If no names are provided, it will display "Hello, World!"
+ *
+ * @author Developer Name
+ * @version 4.0
+ * @since UC1
+ */
+
+public class HelloApp {
+
+    public static void main(String[] args) {
+
+        String message;
+
+        // Check if arguments are provided
+        if (args.length == 0) {
+            message = "World";
+        } else {
+            // Build a string of names
+            StringBuilder nameBuilder = new StringBuilder();
+
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+
+            message = nameBuilder.toString();
+        }
+
+        System.out.println("Hello, " + message + "!");
+    }
+}
