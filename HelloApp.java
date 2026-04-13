47459cebaad1f9ed1066d34b09de275f6bcdbe6d
pritikasharma1979-jpg <pritikasharma1979@gmail.com>
2026-04-13 00:24:33 -0400

    Coded UC4: Display Hello with Multiple Command-Line Arguments

--- HelloApp.java
+++ HelloApp.java
@@ -1,12 +1,22 @@
 public class HelloApp {
     public static void main(String[] args) {
 
+        String name = "World";
+
         if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
-        } else {
-            System.out.println("Hello, World!");
+            StringBuilder nameBuilder = new StringBuilder();
+
+            for (int i = 0; i < args.length; i++) {
+                nameBuilder.append(args[i]);
+
+                if (i < args.length - 1) {
+                    nameBuilder.append(", ");
+                }
+            }
+
+            name = nameBuilder.toString();
         }
 
+        System.out.println("Hello, " + name + "!");
     }
 }
